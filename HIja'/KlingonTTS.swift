6b6b4ffbd3a8efc66dcd67858bfb85555c29be30
Daniel Dadap <daniel@dadap.net>
2018-03-30 09:15:41 -0400

    Add Klingon transliteration functionality

--- HIja'/KlingonTTS.swift
+++ HIja'/KlingonTTS.swift
@@ -9,6 +9,105 @@
 import UIKit
 import AVKit
 
+class KlingonTransliterator {
+    private class KlingonLetter {
+        final var latin : String
+        final var xifankQ : String
+        final var xifankq : String
+        final var pIqaD : String
+
+        init(_ l : String, _ kQ : String, _ kq : String, _ p : String) {
+            latin = l
+            xifankQ = kQ
+            xifankq = kq
+            pIqaD = p
+        }
+    }
+
+    private static let lettertable = [
+        KlingonLetter("a", "a", "a", ""),
+        KlingonLetter("b", "b", "b", ""),
+        KlingonLetter("ch", "c", "c", ""),
+        KlingonLetter("D", "d", "d", ""),
+        KlingonLetter("e", "e", "e", ""),
+        // Temporarily munge "gh" to "G", to prevent "ngh" from becoming "f" instead of "fg".
+        KlingonLetter("gh", "G", "G", ""),
+        KlingonLetter("H", "h", "h", ""),
+        KlingonLetter("I", "i", "i", ""),
+        KlingonLetter("j", "j", "j", ""),
+        KlingonLetter("l", "l", "l", ""),
+        KlingonLetter("m", "m", "m", ""),
+        KlingonLetter("n", "n", "n", ""),
+        KlingonLetter("ng", "f", "f", ""),
+        KlingonLetter("o", "o", "o", ""),
+        KlingonLetter("p", "p", "p", ""),
+        KlingonLetter("q", "q", "k", ""),
+        KlingonLetter("Q", "k", "q", ""),
+        KlingonLetter("r", "r", "r", ""),
+        KlingonLetter("S", "s", "s", ""),
+        KlingonLetter("t", "t", "t", ""),
+        KlingonLetter("tlh", "x", "x", ""),
+        KlingonLetter("u", "u", "u", ""),
+        KlingonLetter("v", "v", "v", ""),
+        KlingonLetter("w", "w", "w", ""),
+        KlingonLetter("y", "y", "y", ""),
+        KlingonLetter("'", "z", "z", ""),
+    ]
+
+    enum KlingonScript {
+        case latin
+        case xifanholkQ
+        case xifanholkq
+        case pIqaD
+    }
+
+    static func transliterate(_ fromString : String, from: KlingonScript = .latin, to: KlingonScript = .xifanholkQ) -> String {
+        var ret : String = fromString
+
+        if (from == .xifanholkq || from == .xifanholkQ) {
+            // The transliteration table has the munged "G" in the xifan hol entries;
+            // munge it back before transliterating from xifan hol
+            ret = ret.replacingOccurrences(of: "g", with: "G")
+        }
+
+        for letter in lettertable {
+            let fromLetter : String
+            let toLetter : String
+
+            switch(from) {
+            case .latin:
+                fromLetter = letter.latin
+            case .xifanholkQ:
+                fromLetter = letter.xifankQ
+            case .xifanholkq:
+                fromLetter = letter.xifankq
+            case .pIqaD:
+                fromLetter = letter.pIqaD
+            }
+
+            switch(to) {
+            case .latin:
+                toLetter = letter.latin
+            case .xifanholkQ:
+                toLetter = letter.xifankQ
+            case .xifanholkq:
+                toLetter = letter.xifankq
+            case .pIqaD:
+                toLetter = letter.pIqaD
+            }
+
+            ret = ret.replacingOccurrences(of: fromLetter, with: toLetter)
+        }
+
+        if (to == .xifanholkq || to == .xifanholkQ) {
+            // Restore 'G' to 'g'
+            ret = ret.replacingOccurrences(of: "G", with: "g")
+        }
+
+        return ret
+    }
+}
+
 class KlingonTTS: NSObject {
     enum TTSError : Error {
         case assetNotFound
